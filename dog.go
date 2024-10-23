eb10152ff3af83fa488bdf7375b2c1ec158e630f
arpit.g1 <arpit.g@kinaracapital.com>
2024-10-23 06:56:05 +0530

    init

---
+++ dog.go
@@ -0,0 +1,7 @@
+import (
+	"strings"
+)
+
+func WhenGrowsUp(s string) string {
+	return "When the pup grows up it says" + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Arpitmovers/dog
+
+go 1.22.2
